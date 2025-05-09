055da1a86e71f0343c7e33ebf6ad5c9978476299
Fengwei Du <dufengwei@mail.ustc.edu.cn>
2025-05-09 23:12:52 +0800

    Add files via upload

---
+++ 02_DIPwithPyTorch/Pix2Pix/FCN_network.py
@@ -0,0 +1,82 @@
+import torch.nn as nn
+
+class FullyConvNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+         # Encoder (Convolutional Layers)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=4, stride=2, padding=1),  # Input channels: 3, Output channels: 8
+            nn.BatchNorm2d(8),
+            nn.ReLU(inplace=True)
+        )
+        ### FILL: add more CONV Layers
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        
+        # Decoder (Deconvolutional Layers)
+        ### FILL: add ConvTranspose Layers
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(inplace=True)
+        )
+        self.deconv5 = nn.Sequential(
+            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh() #输出值[-1,1]
+        )
+        ### Note: since last layer outputs RGB channels, may need specific activation function
+
+    def forward(self, x):
+        # Encoder forward pass
+        
+        # Decoder forward pass
+        
+        ### FILL: encoder-decoder forward pass
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        x = self.deconv3(x)
+        x = self.deconv4(x)
+        x = self.deconv5(x)
+        output = x
+        
+        return output
+    
